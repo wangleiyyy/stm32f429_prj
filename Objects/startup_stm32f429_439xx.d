f34c4e660b2454a90e38f1035b3a326004d18ee5
wangleiyyy <wanglei_sins@yahoo.com>
2016-06-18 02:10:16 +0800

    add iginore test

--- Objects/startup_stm32f429_439xx.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_stm32f429_439xx.o: CMSIS\Device\ST\STM32F4xx\Source\Templates\arm\startup_stm32f429_439xx.s
--- Objects/startup_stm32f429xx.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_stm32f429xx.o: RTE\Device\STM32F429IGTx\startup_stm32f429xx.s
